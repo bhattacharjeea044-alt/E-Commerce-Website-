1b1655b7cac5bef2c7e55b46274529d7f8ae8e4b
bhattacharjeea044-alt <bhattacharjeea044@gmail.com>
2026-08-25 06:29:52 +0530

    Add Footer component with navigation links

---
+++ Footer.jsx
@@ -0,0 +1,45 @@
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <div>
+        <h2>ShopKart</h2>
+
+        <p>
+          Your one-stop online shopping destination.
+        </p>
+      </div>
+
+      <div>
+        <h3>Quick Links</h3>
+
+        <p>Home</p>
+        <p>Products</p>
+        <p>Contact</p>
+      </div>
+
+      <div>
+        <h3>Customer Service</h3>
+
+        <p>Help Center</p>
+        <p>Returns</p>
+        <p>Shipping</p>
+      </div>
+
+      <div>
+        <h3>Follow Us</h3>
+
+        <p>Facebook</p>
+        <p>Instagram</p>
+        <p>YouTube</p>
+      </div>
+
+      <div className="copyright">
+        © 2026 ShopKart. All Rights Reserved.
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
