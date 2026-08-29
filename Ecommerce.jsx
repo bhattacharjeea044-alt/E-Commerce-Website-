29e84316834c1c6114d7c2076fbc50e18a7636dd
bhattacharjeea044-alt <bhattacharjeea044@gmail.com>
2026-08-29 09:22:11 +0530

    Create Ecommerce.jsx

---
+++ Ecommerce.jsx
@@ -0,0 +1,16 @@
+import "./Ecommerce.css";
+
+function Ecommerce() {
+  return (
+    <div className="ecommerce">
+      <h1>E-Commerce Website</h1>
+
+      <img
+        src="/ecommerce-user-admin.png"
+        alt="E-Commerce User and Admin Panel"
+      />
+    </div>
+  );
+}
+
+export default Ecommerce;
